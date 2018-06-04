fc1f4bcdd0de1ffd1a2ce01c1630dcd5fca3efe9
cheetah747 <cheetah747@qq.com>
2018-06-04 22:30:15 +0800

    这是Dev分支

--- app/src/main/java/com/sibyl/gittest/MainActivity.kt
+++ app/src/main/java/com/sibyl/gittest/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //这是Master分支
+        //这是Dev分支
     }
 }
